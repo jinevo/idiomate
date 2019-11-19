cc18316550482fd8424d30a9ed50f41953992173
jnevo <jeannoel.evo@gmail.com>
2019-11-19 21:47:25 +0100

    Idiomate working base

---
+++ src/views/Challenge.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="Challenge">
+    <v-flex class="flex-column justify-center align-center">
+      <!-- <v-btn :disabled="audioActive" @click="startAudio">Put the mic on !</v-btn> -->
+      <h1>
+        <v-icon>mid-microphone</v-icon>Say
+      </h1>
+      <br />
+      <strong>"{{ sentencesToMath[currentSentenceIndex] }}"</strong>
+      <v-chip
+        v-for="(transcript, index) of transcriptions"
+        :key="index"
+        :color="transcriptionMatched(transcript) ? 'green' : 'red'"
+        class="ma-2"
+      >{{transcript}}</v-chip>
+    </v-flex>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+declare global {
+  interface Window {
+    webkitSpeechRecognition: SpeechRecognition | undefined;
+  }
+}
+
+export default Vue.extend({
+  name: "home",
+  data: () => {
+    return {
+      currentSentenceIndex: 0,
+      sentencesToMath: [
+        "Oui",
+        "Voiture",
+        "Je suis le meilleur",
+        "Je collectionne les papillons",
+        "Hier je suis allé voir un film au cinéma"
+      ],
+      successSentences: [] as string[],
+      transcriptions: [] as string[],
+      audioActive: false,
+      recognition: (null as unknown) as SpeechRecognition
+    };
+  },
+  components: {
+    // HelloWorld
+  },
+  mounted() {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    this.recognition = new SpeechRecognition();
+
+    //this.recognition.interimResults = true;
+    this.recognition.lang = "fr-FR";
+    this.recognition.continuous = true;
+
+    this.recognition.onresult = event => {
+      const lastTranscription = [...event.results].pop()[0].transcript;
+      this.transcriptions.push(lastTranscription);
+
+      if (this.transcriptionMatched(lastTranscription)) {
+        if (this.currentSentenceIndex === this.sentencesToMath.length - 1) {
+          this.recognition.stop();
+        } else {
+          this.successSentences.push(lastTranscription);
+          this.currentSentenceIndex++;
+          this.transcriptions = [];
+        }
+      }
+    };
+
+    this.recognition.onaudiostart = event => {
+      this.audioActive = true;
+    };
+    this.recognition.onaudioend = event => {
+      this.audioActive = false;
+      console.log("onaudioend");
+    };
+    this.recognition.onspeechend = event => {};
+    this.recognition.onerror = event => {
+      this.audioActive = false;
+    };
+    this.recognition.onnomatch = event => {
+      this.audioActive = false;
+    };
+
+    this.recognition.start();
+  },
+  methods: {
+    startAudio() {
+      this.recognition.start();
+    },
+    transcriptionMatched(transcription: string): boolean {
+      return (
+        transcription.trim() ===
+        this.sentencesToMath[this.currentSentenceIndex].toLowerCase()
+      );
+    }
+  }
+});
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>Idiomate everywhere with anyone !</h1>
+    <p>Who has the best international pronounciation ?</p>
+    <v-select label="Pick-up a language" :items="[{text: 'French', value: 'fr_FR'}]" />
+    <v-btn primary @click="play">Play</v-btn>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<script lang="ts">
+import Vue from "vue";
 
-export default {
-  name: "home",
-  components: {
-    HelloWorld
+declare global {
+  interface Window {
+    webkitSpeechRecognition: SpeechRecognition | undefined;
   }
-};
+}
+
+export default Vue.extend({
+  name: "home",
+  data: () => {
+    return {
+      text: "",
+      recognition: (null as unknown) as SpeechRecognition
+    };
+  },
+  components: {
+    // HelloWorld
+  },
+  mounted() {
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
+    this.recognition = new SpeechRecognition();
+
+    this.recognition.interimResults = true;
+    this.recognition.lang = "es-PE";
+
+    this.recognition.onresult = event => {
+      this.text = event.results[0][0].transcript;
+      console.log(event.results[0][0].transcript);
+    };
+  },
+  methods: {
+    play() {
+      this.$router.replace({ name: "challenge" });
+    }
+  }
+});
 </script>
